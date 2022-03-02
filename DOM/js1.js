8c09baf9275fcd67c7823933251d09d17e6fc92b
Ion-Gîrbu <71259649+IonGirbu@users.noreply.github.com>
2022-03-02 11:28:04 +0200

    Update js1.js

--- DOM/js1.js
+++ DOM/js1.js
@@ -7,3 +7,25 @@ elem.innerHTML = "Orange";
 elem.addEventListener("click", ()=>{
     document.bgColor = "Orange";
 });
+let counter = 0;
+const ol_list = document.querySelector(".list");
+
+listCreation = setInterval(() => {
+
+    let li = document.createElement("li");
+    li.textContent = `Элемент ${counter + 1}`;
+    ol_list.append(li);
+
+    if (counter % 2 == 0) {
+        li.style.backgroundColor = "green";
+    } else {
+        li.style.backgroundColor = "violet";
+    }
+
+    if (counter == 19) {
+
+        clearInterval(listCreation);
+
+    }
+    counter++;
+}, 100);
